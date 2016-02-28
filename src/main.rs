84aa1e2d78ee0b6c5565aa341c4b707668ebd683
Tomo <yubizume@gmail.com>
2016-02-28 17:07:48 +0100

    Representing a board

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate core;
 use core::fmt;
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum Mark {
     X,
     O
@@ -13,7 +13,7 @@ impl fmt::Display for Mark {
     }
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone)]
 enum Field {
     Empty,
     M(Mark)
@@ -25,10 +25,31 @@ impl fmt::Display for Field {
     }
 }
 
+#[derive(Debug, Copy, Clone)]
+struct Board {
+    fields: [Field; 9]
+}
+
+impl Board {
+    pub fn new() -> Board {
+        Board { fields: [Field::Empty; 9]}
+    }
+}
+
+impl fmt::Display for Board {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", self.fields)
+    }
+}
+
+
 fn main() {
     let f1 : Field = Field::Empty;
     let f2 : Field = Field::M(Mark::X);
     let f3 : Field = Field::M(Mark::O);
 
-    println!("f1: {:?}, f2: {:?}, f3: {:?}", f1, f2, f3);
+    let b : Board = Board::new();
+
+    println!("f1: {}, f2: {}, f3: {}", f1, f2, f3);
+    println!("Board: {}", &b)
 }
